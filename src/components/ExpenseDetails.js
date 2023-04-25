2d8c601de49c7425c447157851119e95ddc804e9
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-04-25 11:14:14 +0530

    totally  updated

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,15 +1,34 @@
-import './ExpenseDetails'
-import './Card.css';
-function ExpenseDetails(props)
-{ var title=props.title;
-  var amount=props.amount
-  var id=props.id;
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+import './ExpenseDetails.css';
 
-  return <div className="card">
-    <div >{title}</div>
-    <div>{amount}</div>
-    <div>{id}</div>
-  </div>
-
+function ExpenseDetails(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
 }
-export default  ExpenseDetails
+
+export default ExpenseDetails;
+
+
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,20 @@
-import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
 import Card from './Card';
-function ExpenseItem(props)
-{ return  <Card className='expense-item'>
-    <div>Expense Items</div>
-    <ExpenseDate date={props.date}></ExpenseDate>
-    <ExpenseDetails title={props.title} amount={props.children} id={"id:"+props.id}/>
-     </Card>
+import './ExpenseItem.css';
 
-
-
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
+      </div>
+    </Card>
+  );
 }
-export default ExpenseItem
+
+export default ExpenseItem;
+
+
+
