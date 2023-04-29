d2bcaa376bf24120fe3f3dc6f36b60a5bf1f61e0
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-04-29 23:19:43 +0530

    two-way-binding-manystates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,81 +1,85 @@
 import { useState } from 'react';
 import './ExpenseForm.css'
 
+
+//NOTE:COMMENTED THINGS ARE BINDING WITH SINGLE STATE 
+
 function ExpenseForm()
 {  //using multiple states
     
-    // const [title,settitle]=useState('')
-    // const [amount,setamount]=useState('')
-    // const[date,setdate]=useState('')
+    const [title,settitle]=useState('')
+    const [amount,setamount]=useState('')
+    const[date,setdate]=useState('')
+
 
  
 //using single state
 
-const [inputs,setfunction]=useState({
-    enteredtitle:'',
-    entereddate:'',
-    enteredamount:''
-})
+// const [inputs,setfunction]=useState({
+//     enteredtitle:'',
+//     entereddate:'',
+//     enteredamount:''
+// })
 
 
    const placetitle=(event)=>{
-    // settitle(event.target.value)
+     settitle(event.target.value)
 
-       setfunction((prevState)=>{
-       return {  ...prevState,
-            enteredtitle:event.target.value
+    //    setfunction((prevState)=>{
+    //    return {  ...prevState,
+    //         enteredtitle:event.target.value
 
-        }
-       })
+    //     }
+    //    })
    }
    const placeamount=(event)=>{
 
-    // setamount(event.target.value)
+    setamount(event.target.value)
 
-    setfunction((prevState)=>{
-        return {  ...prevState,
-             enteredamount:event.target.value
+    // setfunction((prevState)=>{
+    //     return {  ...prevState,
+    //          enteredamount:event.target.value
  
-         }
-        })
+    //      }
+    //     })
  
     
 }
 const placedate=(event)=>{
 
-    // setdate(event.target.value)
+     setdate(event.target.value)
 
-    setfunction((prevState)=>{
-        return {  ...prevState,
-             entereddate:event.target.value
+    // setfunction((prevState)=>{
+    //     return {  ...prevState,
+    //          entereddate:event.target.value
  
-         }
-        })
+    //      }
+    //     })
    
 }
 const show=(e)=>{
     e.preventDefault();
 
-//     let obj1={amount:amount,
-//         date:date,
-//         title: amount
-// }
-// console.log(obj1)
-// setamount('')
-// setdate('')
-// settitle('')
+    let obj1={amount:amount,
+        date:date,
+        title: amount
+}
+console.log(obj1)
+setamount('')
+setdate('')
+settitle('')
 
 
-    let obj={amount:inputs.enteredamount,
-            date:inputs.entereddate,
-            title: inputs.enteredtitle
-    }
-    console.log(obj);
-    setfunction({
-        enteredamount:'',
-        enteredtitle:'',
-        entereddate:''
-    })
+    // let obj={amount:inputs.enteredamount,
+    //         date:inputs.entereddate,
+    //         title: inputs.enteredtitle
+    // }
+    // console.log(obj);
+    // setfunction({
+    //     enteredamount:'',
+    //     enteredtitle:'',
+    //     entereddate:''
+    // })
 }
 
 
@@ -84,10 +88,10 @@ const show=(e)=>{
 
     return <div>
         <form  className='exp' onSubmit={show}>
-<label>title: </label><input type="text" value={inputs.enteredtitle}  onChange={placetitle} ></input>
-    <label> date: </label><input type="date" value={inputs.entereddate} onChange={placedate}></input>
+<label>title: </label><input type="text" value={title}  onChange={placetitle} ></input>
+    <label> date: </label><input type="date" value={date} onChange={placedate}></input>
     <label> expense value: </label>
-    <input type="text" value={inputs.enteredamount} onChange={placeamount} ></input>
+    <input type="text" value={amount} onChange={placeamount} ></input>
         <button >Add expense </button>
         
     <br/>
